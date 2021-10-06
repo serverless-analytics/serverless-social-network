b727090999600652b05065d2511960ab527acada
mania.abdi <mania.abdi287@gmail.com>
2021-10-06 19:26:58 +0000

    just disable the cache for now

--- functions/read_post/action.py
+++ functions/read_post/action.py
@@ -61,7 +61,7 @@ def execute(args, worker=None):
             if post == -1: 
                 miss = 'y'
                 post = post_collection.find_one(filter={'post_id': post_id})
-                evicted = lru_cache.put(post_id, post)
+                #evicted = lru_cache.put(post_id, post)
             object_access.append({'oid': post_id , 'miss': miss, 'evicted': evicted, 'name': lru_cache.name})
     
             medias = list()
